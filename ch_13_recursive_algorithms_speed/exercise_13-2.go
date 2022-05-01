d36b313d6446a41586b0dab1ef4efce10b777406
Andrew Moore <andrewdmoore84@gmail.com>
2022-05-01 19:51:01 -0400

    Fix spacing typo in exercise 13.2

--- ch_13_recursive_algorithms_speed/exercise_13-2.go
+++ ch_13_recursive_algorithms_speed/exercise_13-2.go
@@ -54,7 +54,7 @@ func findMissingNumber(nums []int) int {
 }
 
 func main() {
-  fmt.Println(findMissingNumber([]int{2, 3, 1, 4, 5, 0, 7}))   // 6
+        fmt.Println(findMissingNumber([]int{2, 3, 1, 4, 5, 0, 7}))   // 6
 	fmt.Println(findMissingNumber([]int{0, 1, 2, 3, 5, 6}))      // 4
 	fmt.Println(findMissingNumber([]int{6, 5, 4, 1, 0, 2}))      // 3
 	fmt.Println(findMissingNumber([]int{7, 0, 2, 4, 5, 3}))      // 1
